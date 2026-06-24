3bbd104853edd2990ab7a0e43b0c32aa4d85978e
GiulioFardella <giulio.fardella@outlook.it>
2026-06-24 09:38:21 +0200

    adds dependencies and base template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar1 from "./components/Navbar1";
 
-function App() {
+function HomeTest() {
   return (
     <main className="app">
       <section className="app__test">
@@ -8,11 +10,21 @@ function App() {
 
         <h1>Base React attiva.</h1>
 
-        <p>
-          Il progetto è pronto per navbar, routing e pagine.
-        </p>
+        <p>Il progetto è pronto per navbar, routing e pagine.</p>
       </section>
     </main>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar1 />
+
+      <Routes>
+        <Route path="/" element={<HomeTest />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
